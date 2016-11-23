1f6986ea50c063067a29bf497be0d2b454020523
jain <vjain40@gatech.edu>
2016-11-23 00:12:38 -0500

    i have no idea why it does not work

--- noob.ino
+++ noob.ino
@@ -15,17 +15,18 @@
 Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
                          SPI_CLOCK_DIVIDER); // you can change this clock speed
 
-#define WLAN_SSID       "iPhone"           // cannot be longer than 32 characters!
-#define WLAN_PASS       "12345678"
+#define WLAN_SSID       "t1"           // cannot be longer than 32 characters!
+#define WLAN_PASS       "abcdefghij"
 // Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
 #define WLAN_SECURITY   WLAN_SEC_WPA2
-#define IDLE_TIMEOUT_MS  3000      // Amount of time to wait (in milliseconds) with no data 
+#define IDLE_TIMEOUT_MS  2000      // Amount of time to wait (in milliseconds) with no data 
 // received before closing the connection.  If you know the server
 // you're accessing is quick to respond, you can reduce this value.
 
 int32_t ip;
 // What page to grab!
-#define WEBSITE      "epa4012.herokuapp.com"
+#define WEBSITE        "epa4012.herokuapp.com"
+//#define WEBSITE      "193.168.43.213"
 #define WEBPAGE      "/"
 
 
@@ -58,44 +59,64 @@ int audioPin = 10;
 void setup() {
   //pinMode(wifiPower, OUTPUT);
   //pinMode(sdPower, OUTPUT);
-  //noob();
-  analogWrite(A9, 1023);
-  delay(2000);
   Serial.begin(115200);
+  noob();
+  /*analogWrite(A9, 1023);
+    delay(2000);
+    Serial.begin(115200);
 
-  // put your setup code here, to run once:
-  tmrpcm.speakerPin = 11; //11 on Mega, 9 on Uno, Nano, etc
-  if (!SD.begin(SD_ChipSelectPin)) {  // see if the card is present and can be initialized:
+    // put your setup code here, to run once:
+    tmrpcm.speakerPin = 11; //11 on Mega, 9 on Uno, Nano, etc
+    if (!SD.begin(SD_ChipSelectPin)) {  // see if the card is present and can be initialized:
     Serial.println("SD fail");
     return;   // don't do anything more if not
-  }
-  Serial.println("SD works");
+    }
+    Serial.println("SD works");*/
 }
 
 void noob() {
-  //digitalWrite(wifiPower, HIGH);
-  //Serial.println(F("hey"));
-  //analogWrite(A8, 1023);
-  //delay(5000);
-#if !defined(SOFTWARE_SPI)
-  if (ADAFRUIT_CC3000_CS != 53) {
-    pinMode(53, OUTPUT); // SS on Uno, etc.
-  }
-#endif
-
-  Serial.begin(115200);
+  // picture
   if (cam.begin()) {
     Serial.println(F("Camera Found:"));
   } else {
     Serial.println(F("No camera found?"));
     return;
   }
+  cam.setImageSize(VC0706_160x120);
+  delay(2000);
+  if (! cam.takePicture()) {
+    Serial.println(F("Failed to snap!"));
+    return;
+  }  else {
+    Serial.println(F("Picture taken!"));
+  }
+  uint8_t* buffer;
+  int16_t jpglen = cam.frameLength();
+  Serial.println(jpglen);
 
-  //cam.setImageSize(VC0706_640x480);        // biggest
-  //cam.setImageSize(VC0706_320x240);        // medium
-  cam.setImageSize(VC0706_160x120);          // small
-
-  Serial.println("whats going on?");
+  uint8_t arr[jpglen];
+  int i = 0;
+  while (jpglen > 0) {
+    uint8_t * buffer;
+    uint8_t bytesToRead = min(64, jpglen); // change 32 to 64 for a speedup but may not work with all setups!
+    buffer = cam.readPicture(bytesToRead);
+    for (int j = 0; j < bytesToRead; j++) {
+      arr[i] = buffer[j];
+      i++;
+    }
+    Serial.println(i);
+    jpglen -= bytesToRead;
+  }
+  Serial.println("cmon bby");
+  Serial.println(arr[0]);
+  //String myString = String((char *) arr);
+  //Serial.println(myString);
+  // wifi stuff
+#if !defined(SOFTWARE_SPI)
+  if (ADAFRUIT_CC3000_CS != 53) {
+    pinMode(53, OUTPUT); // SS on Uno, etc.
+  }
+#endif
 
   if (!cc3000.begin()) {
     Serial.println(F("Couldn't begin()! Check your wiring?"));
@@ -111,9 +132,6 @@ void noob() {
     while (1);
   }
 
-  // Optional SSID scan
-  // listSSIDResults();
-
   Serial.print(F("\nAttempting to connect to ")); Serial.println(WLAN_SSID);
   if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
     Serial.println(F("Failed!"));
@@ -123,7 +141,7 @@ void noob() {
   Serial.println(F("Connected!"));
 
   /* Wait for DHCP to complete */
-  Serial.println(F("  "));
+  Serial.println(F("DHCP"));
   while (!cc3000.checkDHCP())
   {
     delay(100); // ToDo: Insert a DHCP timeout!
@@ -133,9 +151,7 @@ void noob() {
   while (! displayConnectionDetails()) {
     delay(1000);
   }
-
   ip = 0;
-  // Try looking up the website's IP address
   Serial.print(WEBSITE); Serial.print(F(" -> "));
   while (ip == 0) {
     if (! cc3000.getHostByName(WEBSITE, &ip)) {
@@ -143,36 +159,10 @@ void noob() {
     }
     delay(500);
   }
-
   cc3000.printIPdotsRev(ip);
-
-
-
-
-
-  // You can read the size back from the camera (optional, but maybe useful?)
-  uint8_t imgsize = cam.getImageSize();
-  //Serial.println(F("Snap in 2 secs..."));
-  delay(2000);
-
-  if (! cam.takePicture())
-    Serial.println(F("Failed to snap!"));
-  else
-    Serial.println(F("Picture taken!"));
-
-  // Get the size of the image (frame) taken
-  int32_t time = millis();
-  pinMode(8, OUTPUT);
-
-  uint8_t* buffer;
-  int16_t jpglen = cam.frameLength();
-  Serial.println(jpglen);
-  uint8_t bytesToRead = min(32, jpglen); // change 32 to 64 for a speedup but may not work with all setups!
-  // DO NOT USE 128 BYTES
-
+  Serial.println(ip);
   Adafruit_CC3000_Client client = cc3000.connectTCP(ip, 80);
   if (client.connected()) {
-    //Adafruit_CC3000_ClientRef client = server.available();
     if (client) {
       Serial.println(F("heya"));
       client.println("POST /oct HTTP/1.1");
@@ -182,24 +172,10 @@ void noob() {
       client.println("Connection: keep-alive");
       client.println("Content-Type: application/octet-stream");
       client.print("Content-Length: ");
-      //client.println(4);
-      client.println(jpglen);
-      //client.println(480);
+      client.println(i);
       client.println();
-      int i = 0;
-      //while (i < 15) {
-        //i++;
-        while (jpglen > 0) {
-        // read 32 bytes at a time;
-        uint8_t *buffer;
-        uint8_t bytesToRead = min(32, jpglen); // change 32 to 64 for a speedup but may not work with all setups!
-        buffer = cam.readPicture(bytesToRead);
-        //client.print((char *)buffer);
-        Serial.println(client.write(buffer, bytesToRead));
-        //Serial.write((char *)buffer, bytesToRead);
-        jpglen -= bytesToRead;
-        Serial.println(jpglen);
-      }
+      Serial.write(arr, i);
+      client.write(arr, i);
       client.flush();
       Serial.println(F("sent"));
       /*Read data until either the connection is closed, or the idle timeout is reached. */
@@ -220,25 +196,20 @@ void noob() {
   }
 
   Serial.write(0);
-  //Serial.println();
 
-
-  time = millis() - time;
-  //Serial.print(time); Serial.println(" ms elapsed");
-  //digitalWrite(wifiPower, LOW);
-  //analogWrite(A8, 0);
-  //delay(500);
 }
 
 void loop() {
+  //Serial.println("yoyo");
+  //delay(1000);
   // put your main code here, to run repeatedly:
-  Serial.println(analogRead(0));
-  if (analogRead(0) < 400) {
+  /*Serial.println(analogRead(0));
+    if (analogRead(0) < 400) {
     tmrpcm.play("BUGSBU~1.WAV");
     delay(3000);
     Serial.println("dance");
-  }
-  delay(100);
+    }
+    delay(100);*/
 }
 
 bool displayConnectionDetails(void) {
